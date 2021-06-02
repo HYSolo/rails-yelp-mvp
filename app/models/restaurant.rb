f9a4545e6d6609a398cd698160cae47bd93e6922
Hillel Solomon <solomonhillel@gmail.com>
2021-06-02 14:42:15 +1000

    added validations to reviews and restaurants

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,5 @@
 class Restaurant < ApplicationRecord
-  has_many :reviews dependent: :destroy
+  has_many :reviews, dependent: :destroy
+  validates_presence_of :name, :address, :category
+  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"] }
 end
