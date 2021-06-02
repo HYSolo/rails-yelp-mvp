a0f27fe4721310216f034953e2697df0221c8443
Hillel Solomon <solomonhillel@gmail.com>
2021-06-02 14:53:28 +1000

    rails seed with restaurant data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+5.times do
+name = Faker::Restaurant.name
+address = "#{Faker::Address.street_address}, #{Faker::Address.community}"
+phone_number = Faker::PhoneNumber.cell_phone
+category = ["chinese", "italian", "japanese", "french", "belgian"].sample
+Restaurant.create!(name: name, address: address, phone_number: phone_number, category: category)
+end
