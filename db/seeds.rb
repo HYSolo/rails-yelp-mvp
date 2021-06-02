dcb373ec57ce15870bdd15063d937ad724ce47fd
Hillel Solomon <solomonhillel@gmail.com>
2021-06-02 17:49:07 +1000

    populated db with reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 5.times do
-name = Faker::Restaurant.name
-address = "#{Faker::Address.street_address}, #{Faker::Address.community}"
-phone_number = Faker::PhoneNumber.cell_phone
-category = ["chinese", "italian", "japanese", "french", "belgian"].sample
-Restaurant.create!(name: name, address: address, phone_number: phone_number, category: category)
+  name = Faker::Restaurant.name
+  address = "#{Faker::Address.street_address}, #{Faker::Address.community}"
+  phone_number = Faker::PhoneNumber.cell_phone
+  category = ["chinese", "italian", "japanese", "french", "belgian"].sample
+  Restaurant.create!(name: name, address: address, phone_number: phone_number, category: category)
+end
+
+40.times do
+  restaurant = Restaurant.all.sample
+  rating = rand(0..5)
+  content = Faker::Restaurant.review
+  review = Review.new(rating: rating, content: content)
+  review.restaurant = restaurant
+  review.save
 end
